db241a4a52010a1d8aa08f261f6e12ab2d7b0250
dyn64 <dynamical@gmail.com>
2026-09-06 13:05:41 +0200

    1.4 fix

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading input:", err)
-		}
-		cleaned := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", cleaned[0])
-	}
-
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading input:", err)
+		}
+		cleaned := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
