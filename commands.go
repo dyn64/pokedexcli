12bd69150efc1f8cf6ec2e874c280519f881461f
dyn64 <dynamical@gmail.com>
2026-09-06 14:46:36 +0200

    1.5

---
+++ commands.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	for _, cmd := range getCmd() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -7,7 +7,42 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// var commands = map[string]cliCommand{}
+
+func getCmd() map[string]cliCommand {
+	// commands = make(map[string]cliCommand)
+	// commands["exit"] = cliCommand{
+	// 	name:        "exit",
+	// 	description: "Exit the Pokedex",
+	// 	callback:    commandExit,
+	// }
+	// commands["help"] = cliCommand{
+	// 	name:        "help",
+	// 	description: "Displays a help message",
+	// 	callback:    commandHelp,
+	// }
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func startRepl() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -16,14 +51,19 @@ func startRepl() {
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading input:", err)
 		}
-		cleaned := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", cleaned[0])
+		input := cleanInput(scanner.Text())
+		cmd, exists := getCmd()[input[0]]
+		if exists {
+			cmd.callback()
+		} else {
+			fmt.Printf("Unknown command: %s\n", input[0])
+		}
+
 	}
 }
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	result := strings.Fields(text)
-
 	return result
 }
